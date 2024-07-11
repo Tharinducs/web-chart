7b372f68494fed66aa59fab69162dc10ae176ecf
Tharindu Sandaruwan <tharindusandaruwan40@gmail.com>
2024-07-11 22:19:43 +0800

    project refactor with API changes

---
+++ chart-api/Controllers/CreditDataController.cs
@@ -0,0 +1,113 @@
+﻿using chart_data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chart_data.Controllers;
+
+[ApiController]
+[Route("api/credit")]
+public class CreditDataController : ControllerBase
+{
+
+    private readonly ILogger<CreditDataController> _logger;
+    private readonly List<CreditData> _creditData;
+
+    public CreditDataController(ILogger<CreditDataController> logger)
+    {
+        _logger = logger;
+        _creditData = new List<CreditData>
+           {
+                new CreditData
+                {
+                    Value = "6M",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/24", Value = 10 },
+                        new ChartData { Date = "03/24", Value = 150.30 },
+                        new ChartData { Date = "04/20", Value = 250 },
+                        new ChartData { Date = "04/30", Value = 430 },
+                        new ChartData { Date = "05/10", Value = 420.95 },
+                        new ChartData { Date = "05/24", Value = 450.95 }
+                    },
+                    WeightedBond = "1.89 BSD",
+                    AsOf = "2024/05/24"
+                },
+                new CreditData
+                {
+                    Value = "12M",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/24", Value = 20 },
+                        new ChartData { Date = "03/24", Value = 450.30 },
+                        new ChartData { Date = "04/20", Value = 350 },
+                        new ChartData { Date = "04/30", Value = 480 },
+                        new ChartData { Date = "05/10", Value = 490.95 },
+                        new ChartData { Date = "05/24", Value = 550.95 }
+                    },
+                    WeightedBond = "2.89 BSD",
+                    AsOf = "2024/05/24"
+                },
+                new CreditData
+                {
+                    Value = "3Y",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/24", Value = 0 },
+                        new ChartData { Date = "03/24", Value = 250.30 },
+                        new ChartData { Date = "04/20", Value = 350 },
+                        new ChartData { Date = "04/30", Value = 480 },
+                        new ChartData { Date = "05/10", Value = 440.95 },
+                        new ChartData { Date = "05/24", Value = 470 }
+                    },
+                    WeightedBond = "3.89 BSD",
+                    AsOf = "2024/05/24"
+                },
+                new CreditData
+                {
+                    Value = "5Y",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/24", Value = 10 },
+                        new ChartData { Date = "03/24", Value = 150.30 },
+                        new ChartData { Date = "04/20", Value = 250 },
+                        new ChartData { Date = "04/30", Value = 430 },
+                        new ChartData { Date = "05/10", Value = 420.95 },
+                        new ChartData { Date = "05/24", Value = 450.95 }
+                    },
+                    WeightedBond = "2.40 BSD",
+                    AsOf = "2024/05/24"
+                },
+                new CreditData
+                {
+                    Value = "FYTD",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/24", Value = 10 },
+                        new ChartData { Date = "03/24", Value = 150.30 },
+                        new ChartData { Date = "04/20", Value = 250 },
+                        new ChartData { Date = "04/30", Value = 430 },
+                        new ChartData { Date = "05/10", Value = 420.95 },
+                        new ChartData { Date = "05/24", Value = 450.95 }
+                    },
+                    WeightedBond = "3.20 BSD",
+                    AsOf = "2024/05/24"
+                }
+            };
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<CreditData>> Get()
+    {
+        _logger.LogInformation("get whole chart list");
+        return Ok(_creditData);
+    }
+
+    [HttpGet("{param}")]
+    public ActionResult<IEnumerable<CreditData>> Get(string param)
+    {
+        _logger.LogInformation("get list according to param");
+        var filteredList = _creditData.Where(c => c.Value == param).ToList();
+        _logger.LogInformation($"Filtered list length {filteredList.Count()}");
+        return Ok(filteredList);
+    }
+}
+
---
+++ chart-api/Controllers/StockDataController.cs
@@ -0,0 +1,104 @@
+﻿using chart_data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chart_data.Controllers;
+
+[ApiController]
+[Route("api/stock")]
+public class StockDataController : ControllerBase
+{
+
+    private readonly ILogger<StockDataController> _logger;
+    private readonly List<StockData> _stockData;
+
+    public StockDataController(ILogger<StockDataController> logger)
+    {
+        _logger = logger;
+        _stockData = new List<StockData>
+           {
+                new StockData
+                {
+                    Value = "6M",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/24", Value = 30 },
+                        new ChartData { Date = "03/24", Value = 150.30 },
+                        new ChartData { Date = "04/20", Value = 250 },
+                        new ChartData { Date = "04/30", Value = 430 },
+                        new ChartData { Date = "05/10", Value = 420.95 },
+                        new ChartData { Date = "05/24", Value = 450.95 }
+                    },
+                    LatestPrice = "450.95",
+                },
+                new StockData
+                {
+                    Value = "12M",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/24", Value = 30 },
+                        new ChartData { Date = "03/24", Value = 150.30 },
+                        new ChartData { Date = "04/20", Value = 250 },
+                        new ChartData { Date = "04/30", Value = 430 },
+                        new ChartData { Date = "05/10", Value = 420.95 },
+                        new ChartData { Date = "05/24", Value = 950.95 }
+                    },
+                    LatestPrice = "950.95",
+                },
+                new StockData
+                {
+                    Value = "3Y",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/24", Value = 30 },
+                        new ChartData { Date = "03/24", Value = 350.30 },
+                        new ChartData { Date = "04/20", Value = 450 },
+                        new ChartData { Date = "04/30", Value = 530 },
+                        new ChartData { Date = "05/10", Value = 420.95 },
+                        new ChartData { Date = "05/24", Value = 450.95 }
+                    },
+                    LatestPrice = "450.95",
+                },
+                new StockData
+                {
+                    Value = "5Y",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/01", Value = 230.2 },
+                        new ChartData { Date = "01/02", Value = 231.5 },
+                        new ChartData { Date = "01/03", Value = 228.9 },
+                        new ChartData { Date = "01/04", Value = 233.3 }
+                    },
+                    LatestPrice = "233.3",
+                },
+                new StockData
+                {
+                    Value = "FYTD",
+                    ChartData = new List<ChartData>
+                    {
+                        new ChartData { Date = "01/01", Value = 2800.1 },
+                        new ChartData { Date = "01/02", Value = 2820.4 },
+                        new ChartData { Date = "01/03", Value = 2785.6 },
+                        new ChartData { Date = "01/04", Value = 2815.7 }
+                    },
+                    LatestPrice = "2815.7",
+                },
+            };
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<StockData>> Get()
+    {
+        _logger.LogInformation("get whole chart data list");
+        return Ok(_stockData);
+    }
+
+    [HttpGet("{param}")]
+    public ActionResult<IEnumerable<StockData>> Get(string param)
+    {
+        _logger.LogInformation("get list according to param");
+        var filteredList = _stockData.Where(c => c.Value == param).ToList();
+        _logger.LogInformation($"Filtered list length {filteredList.Count()}");
+        return Ok(filteredList);
+    }
+}
+
